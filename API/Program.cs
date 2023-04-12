d06be02ef29b1f5ff3b3a11dce2b101483e34a0e
yanpatla <tomipatlayan@gmail.com>
2023-04-12 14:33:16 +0300

    conf CORS

--- API/Program.cs
+++ API/Program.cs
@@ -8,11 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
  {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
  });
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  users: any;
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit(): void {
+    this._http.get('https://localhost:5001/api/users').subscribe({
+      next: (res) => (this.users = res),
+      error: (e) => console.log(e),
+      complete: () => console.log('Request Completed'),
+    });
+  }
 }
