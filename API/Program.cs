37f58eaae2b99f8111a2ac46d1f496b6045ec9b1
yanpatla <tomipatlayan@gmail.com>
2023-04-25 13:46:02 +0300

    set token buidler

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using API.Data;
 using API.interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,19 @@ builder.Services.AddDbContext<DataContext>(opt =>
  });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+    {
+        options.TokenValidationParameters =  new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+            ValidateIssuer = false,
+            ValidateAudience =  false
+        };
+    });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
