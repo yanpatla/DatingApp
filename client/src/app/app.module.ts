d57bdcb88e8e82fb1154af3ce4d0c25cf47e7af1
yanpatla <tomipatlayan@gmail.com>
2023-04-28 22:40:55 +0300

    nav component created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
